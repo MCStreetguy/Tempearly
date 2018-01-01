20510df9d81cfae77f8f94e629f1296aac7323a4
Maximilian Schmidt <m.schmidt@obis-concept.de>
2018-01-01 19:20:45 +0100

    WIP

--- src/Tempearly.php
+++ src/Tempearly.php
@@ -6,7 +6,20 @@
  * @author Maximilian Schmidt <maximilianschmidt404@gmail.com>
  * @license MIT
  */
-abstract class Tempearly {
+class Tempearly {
+
+  private $PATH, $EXTENSION;
+
+  /**
+   * Creates a new Tempearly instance.
+   *
+   * @param string $path The template folder path
+   * @param string $extension [optional] The template file extension
+   */
+  public function __construct($path, $extension = '.tpl.html') {
+    $this->$PATH = $path;
+    $this->$EXTENSION = $extension;
+  }
 
   /**
    * Invokes the rendering process of the given template.
@@ -15,21 +28,29 @@ abstract class Tempearly {
    * @param array $context [optional] Additional context variables for template processing
    * @return string The parsed template string
    */
-  public static function invoke($id,$context = null) {
+  public function render($id,$context = null) {
     if(empty($id) || (!empty($context) && !is_array($context))) {
       throw new Exception('Invalid Arguments!');
     }
 
-    $SOURCE = BASE.'/tpl/'.$id.'.tpl.html';
+    $_this = $this;
+
+    $SOURCE = $_this->$PATH.$id.$_this->$EXTENSION;
     if(!file_exists($SOURCE)) {
       throw new Exception('Template file "'.$SOURCE.'" doesn\'t exist or is not readable!');
     }
 
     $tpl = file_get_contents($SOURCE);
 
-    $systemContext = Tempearly::_buildContext();
+    $systemContext = $_this->_buildContext();
 
-    $parsedTpl = preg_replace_callback('/({{)([\w-]+)(}})/',function($matches) use ($systemContext, $context) {
+    $tpl = preg_replace_callback('/({{if )([\w-]+)(}})([\w\W\n]+)({{\/if}})/',function($matches) use ($systemContext, $context) {
+      $condition = $matches[2];
+      $content = $matches[4];
+      $conditionType = gettype($condition);
+    },$tpl);
+
+    $tpl = preg_replace_callback('/({{)([\w-]+)(}})/',function($matches) use ($systemContext, $context) {
       $variableName = $matches[2];
 
       if(is_array($context) && array_key_exists($variableName,$context)) {
@@ -38,16 +59,25 @@ abstract class Tempearly {
         return $systemContext[$variableName];
       } else {
         // TODO: Add default replacement if no value could be found?
+        return '';
       }
     },$tpl);
 
-    return $parsedTpl;
+    $tpl = preg_replace_callback('/({{tpl\()([\w-]+)(\)}})/',function($matches) use ($_this, $context) {
+      $identifier = $matches[2];
+
+      return $_this->render($identifier,$context);
+    },$tpl);
+
+    return $tpl;
   }
 
   /**
+   * Builds up the system context.
    *
+   * @return array The system context
    */
-  private static function _buildContext() {
+  private function _buildContext() {
     $C = array(
       'rule' => '<hr />'
     );
